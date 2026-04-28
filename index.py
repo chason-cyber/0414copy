0413635e2081a10d9f81d47e0b53dcb9b5abefb0
chason-cyber <chason9508@gmail.com>
2026-04-28 11:53:31 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -52,6 +52,7 @@ def index():
     link += "<a href='/read'>讀取firestore資料(搜尋老師)</a><br><hr>"
     link += "<a href='/spider1'>蜘蛛</a><br><hr>"
     link += "<a href='movie'>即將上線電影</a><br><hr>"
+    link += "<a href='movie2'>讀取開眼電影即將上映影片，寫入Firestore</a><br><hr>"
     return link
 
 @app.route("/mis")
@@ -201,6 +202,43 @@ def movie():
     html += '</ul>'
     
     return Response(html, mimetype='text/html; charset=utf-8')
+
+
+@app.route("/movie2")
+def movie2():
+  url = "http://www.atmovies.com.tw/movie/next/"
+  Data = requests.get(url)
+  Data.encoding = "utf-8"
+  sp = BeautifulSoup(Data.text, "html.parser")
+  result=sp.select(".filmListAllX li")
+  lastUpdate = sp.find("div", class_="smaller09").text[5:]
+
+  for item in result:
+    picture = item.find("img").get("src").replace(" ", "")
+    title = item.find("div", class_="filmtitle").text
+    movie_id = item.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+    hyperlink = "http://www.atmovies.com.tw" + item.find("div", class_="filmtitle").find("a").get("href")
+    show = item.find("div", class_="runtime").text.replace("上映日期：", "")
+    show = show.replace("片長：", "")
+    show = show.replace("分", "")
+    showDate = show[0:10]
+    showLength = show[13:]
+
+    doc = {
+        "title": title,
+        "picture": picture,
+        "hyperlink": hyperlink,
+        "showDate": showDate,
+        "showLength": showLength,
+        "lastUpdate": lastUpdate
+      }
+
+    db = firestore.client()
+    doc_ref = db.collection("電影").document(movie_id)
+    doc_ref.set(doc)    
+    return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate 
+
+
 
 
 if __name__ == "__main__":
