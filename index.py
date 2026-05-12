07a57cc3fbda72d730112d345b8cc2d5d1c92ae3
chason-cyber <chason9508@gmail.com>
2026-05-12 11:26:25 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -59,6 +59,8 @@ def index():
     link += "<a href='weather'>輸入欲查詢的縣市</a><br><hr>"
     link += "<a href='rate'>本片分級</a><br><hr>"
     link += "<a href='webhook'>電影</a><br><hr>" 
+    link += "<a href='webhook2'>電影2</a><br><hr>"
+    link += "<a href='webhook3'>電影3</a><br><hr>"
     return link
 
 @app.route("/mis")
@@ -443,6 +445,46 @@ def webhook():
     msg =  req.get("queryResult").get("queryText")
     info = "動作：" + action + "； 查詢內容：" + msg
     return make_response(jsonify({"fulfillmentText": info}))
+
+
+@app.route("/webhook2", methods=["POST"])
+def webhook2():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "動作：" + action + "； 查詢內容：" + msg
+    if (action == "rateChoice"):
+        rate =  req.get("queryResult").get("parameters").get("rate")
+        info = "您選擇的電影分級是：" + rate
+    return make_response(jsonify({"fulfillmentText": info}))
+
+@app.route("/webhook3", methods=["POST"])
+def webhook3():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "動作：" + action + "； 查詢內容：" + msg
+    if (action == "rateChoice"):
+        rate =  req.get("queryResult").get("parameters").get("rate")
+        info = "我是陳宇謙開發的電影聊天機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
+        db = firestore.client()
+        collection_ref = db.collection("電影含分級")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if rate in dict["rate"]:
+                result += "片名：" + dict["title"] + "\n"
+                result += "介紹：" + dict["hyperlink"] + "\n\n"
+        info += result
+    return make_response(jsonify({"fulfillmentText": info}))
+
+
+
 
         
 if __name__ == "__main__":
