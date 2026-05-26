1b7017da2eb26aba9842b4f9c1558cd53bb14bbc
chason-cyber <chason9508@gmail.com>
2026-05-26 11:41:22 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -494,7 +494,7 @@ def webhook3():
         info =  req["queryResult"]["queryText"]
 
         ai_config = types.GenerateContentConfig(
-        max_output_tokens = 500
+        max_output_tokens = 5000
     )
 
 
