a80cfbfe6d4cae8e43aeb899dd6d0d63b7a05bf4
chason-cyber <chason9508@gmail.com>
2026-06-01 16:18:07 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -478,7 +478,7 @@ def webhook3():
     #info = "動作：" + action + "； 查詢內容：" + msg
     if (action == "rateChoice"):
         rate =  req.get("queryResult").get("parameters").get("rate")
-        info = "我是吳柏慶開發的電影聊天機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
+        info = "我是陳宇謙開發的電影聊天機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
         db = firestore.client()
         collection_ref = db.collection("本週新片含分級")
         docs = collection_ref.get()
